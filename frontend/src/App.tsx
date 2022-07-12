475209562d1bb02f5463ba40357c27d869617aec
Victor Martins <vitor_jhoul@hotmail.com>
2022-07-11 21:50:30 -0300

    Commit other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
     return (
         <>
-        <h1>Teste</h1>
-        <NotificationButton />
+            <Header />
+            <main>
+                <section id="sales">
+                    <div className="dsmeta-container">
+                        <SalesCard />
+                    </div>
+
+
+                </section>
+            </main>
+
         </>
     )
 }
