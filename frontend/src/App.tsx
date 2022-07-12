ab0f26a7b84fc2c4ac55e48482fef392a2059dc6
Victor Martins <vitor_jhoul@hotmail.com>
2022-07-11 21:27:15 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
     return (
+        <>
         <h1>Teste</h1>
+        <NotificationButton />
+        </>
     )
 }
 
