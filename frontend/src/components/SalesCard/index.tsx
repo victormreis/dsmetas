5fcd08aad617939448377f925ddc1f4f3d5c538e
Victor Martins <vitor_jhoul@hotmail.com>
2022-07-13 19:22:29 -0300

    Security config

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,7 +1,7 @@
-import NotificationButton from '../NotificationButton'
-import './styles.css'
+import NotificationButton from '../NotificationButton';
+import './styles.css';
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css'
+import 'react-datepicker/dist/react-datepicker.css';
 import { useState } from 'react';
 
 
