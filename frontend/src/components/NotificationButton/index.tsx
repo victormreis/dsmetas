39082469428efb6f0adb386ee4c5e8edcecadb30
Victor Martins <vitor_jhoul@hotmail.com>
2022-07-17 19:34:11 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,24 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 import './styles.css';
 
-export default function NotificationButton() {
+type Props = {
+    saleId: number;
+
+}
+
+function handleClick(id : number){
+    axios.get(`${BASE_URL}/sales/${id}/notification`)
+    .then(resp => {
+        console.log("Deu certo Brother")
+    })
+
+}
+
+export default function NotificationButton({saleId} : Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
